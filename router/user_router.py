4e5ffa3a8b02b2e0db38a4d68f28fc0f6a944ba7
Flowmikro <cloplop091@gmail.com>
2024-01-02 00:25:18 +0300

    Добавил докстринги

--- router/user_router.py
+++ router/user_router.py
@@ -12,31 +12,18 @@ user_router = APIRouter(prefix='/user')
 
 @user_router.get("/", name='user')
 async def home(request: Request, session: AsyncSession = Depends(orm.get_session)):
+    """
+    Страница выводит все данные с UserModel
+    """
     users = await crud.get_records_from_db(model=orm.UserModel, order=orm.UserModel.id, session=session)
     return settings.templates.TemplateResponse("user/index_user.html", {"request": request, "users": users})
 
 
-@user_router.post("/add")
-async def add(
-        request: Request,
-        username: str = Form(...),
-        email: EmailStr = Form(...),
-        hashed_password: str = Form(...),
-        session: AsyncSession = Depends(orm.get_session)
-):
-    await crud.create_a_record_in_the_db(
-        model=orm.UserModel, session=session, username=username, email=email, hashed_password=hashed_password
-    )
-    return RedirectResponse(url=user_router.url_path_for("user"), status_code=status.HTTP_303_SEE_OTHER)
-
-
-@user_router.get("/addnew")
-async def addnew(request: Request):
-    return settings.templates.TemplateResponse("user/create_user.html", {"request": request})
-
-
 @user_router.get("/edit/{user_id}")
 async def edit(request: Request, user_id: int, db: AsyncSession = Depends(orm.get_session)):
+    """
+    Берем одну запись с UserModel, для того чтобы на основе этого выполнять update
+    """
     user = await db.get(orm.UserModel, user_id)
     return settings.templates.TemplateResponse("user/update_user.html", {"request": request, "user": user})
 
@@ -49,6 +36,9 @@ async def update(
         email: EmailStr = Form(...),
         session: AsyncSession = Depends(orm.get_session)
 ):
+    """
+    Обновляем запись в UserModel
+    """
     await crud.update_a_record_in_the_db(
         pk=user_id, model=orm.UserModel, session=session, username=username, email=email
     )
@@ -57,5 +47,8 @@ async def update(
 
 @user_router.get("/delete/{user_id}")
 async def delete(request: Request, user_id: int, session: AsyncSession = Depends(orm.get_session)):
+    """
+    Удаляем запись в UserModel
+    """
     await crud.delete_a_record_in_the_db(pk=user_id, model=orm.UserModel, session=session)
     return RedirectResponse(url=user_router.url_path_for("user"), status_code=status.HTTP_303_SEE_OTHER)
