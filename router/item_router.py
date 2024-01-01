59b7c65480841776e2f3c9160e8d8d7f9dd432f3
Flowmikro <cloplop091@gmail.com>
2024-01-02 00:27:23 +0300

    Добавил докстринги

--- router/item_router.py
+++ router/item_router.py
@@ -11,6 +11,9 @@ item_router = APIRouter(prefix='/items')
 
 @item_router.get("/", name='items')
 async def home(request: Request, session: AsyncSession = Depends(orm.get_session)):
+    """
+    Страница выводит все данные с ItemModel
+    """
     items = await crud.get_records_from_db(model=orm.ItemModel, order=orm.ItemModel.user_id, session=session)
     return settings.templates.TemplateResponse("item/index_item.html", {"request": request, "items": items})
 
@@ -40,6 +43,9 @@ async def edit(
         item_id: int,
         session: AsyncSession = Depends(orm.get_session)
 ):
+    """
+    Берем одну запись с ItemModel, для того чтобы на основе этого выполнять update
+    """
     item = await session.get(orm.ItemModel, item_id)
     return settings.templates.TemplateResponse("item/update_item.html", {"request": request, "item": item})
 
@@ -53,6 +59,9 @@ async def update(
         user_id: int = Form(...),
         session: AsyncSession = Depends(orm.get_session)
 ):
+    """
+    Обновляем запись в ItemModel
+    """
     await crud.update_a_record_in_the_db(
         pk=item_id, model=orm.ItemModel, session=session, item_name=item_name, description=description,
         user_id=int(user_id)
@@ -62,5 +71,8 @@ async def update(
 
 @item_router.get("/delete/{item_id}")
 async def delete(request: Request, item_id: int, session: AsyncSession = Depends(orm.get_session)):
+    """
+    Удаляем запись в ItemModel
+    """
     await crud.delete_a_record_in_the_db(pk=item_id, model=orm.ItemModel, session=session)
     return RedirectResponse(url=item_router.url_path_for("items"), status_code=status.HTTP_303_SEE_OTHER)
